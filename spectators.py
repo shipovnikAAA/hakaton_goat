f69e959ca150e12b29701c3cf7b0ecfb8ea49ddd
quartan <dimov.nkt@gmail.com>
2024-12-19 16:07:28 +0800

    Spectators(помощь зала)
    на вход функции подается число от 0 до 3(номер правильного ответа) функция выводит список 4 чисел - процент голосов зала за вариант

---
+++ spectators.py
@@ -0,0 +1,34 @@
+import time
+import random
+
+
+def spectators(n = 0):
+    random.seed(time.time())
+    correct = n
+    variants = []
+    for i in range(4):
+        if i != n:
+            variants.append(i)
+    uncorrect = variants[random.randint(0, 2)]
+    ans = []
+    lst = 0
+    for i in range(3):
+        ans.append(random.randint(0, 100 - lst))
+        lst += ans[i]
+    ans.append(100 - lst)
+    ans.sort()
+    out = [0, 0, 0, 0]
+    if random.randint(0, 100) > 10:
+        out[correct] = ans[-1]
+        out[uncorrect] = ans[-2]
+    else:
+        out[uncorrect] = ans[-1]
+        out[correct] = ans[-2]
+    ind = 0
+    for i in range(4):
+        if i != correct and i != uncorrect:
+            out[i] = ans[ind]
+            ind += 1
+    return out
+
+
