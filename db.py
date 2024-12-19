3d8fe63b7b56fa569c32dc5d497019212be32d9e
Konstantin <vaskon0306@gmail.com>
2024-12-19 16:41:01 +0800

    add db

---
+++ db.py
@@ -0,0 +1,30 @@
+from supabase import create_client, Client
+
+url: str = "https://kewhguvjbntiivskcfwf.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtld2hndXZqYm50aWl2c2tjZndmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ1OTE0MDYsImV4cCI6MjA1MDE2NzQwNn0.lVtllKPa8aZcSQTrjPnRJfgiLhUOypI_nWKDJp_US1s"
+supabase: Client = create_client(url, key)
+
+def add_player(login, name, rating, password):
+    try:
+        response = ((supabase.table("players")
+                    .insert({"name": name, "rating": rating, "password": password, "login": login}))
+                    .execute())
+        return response
+
+    except Exception as exception:
+        return exception
+
+def get_player(login, password):
+    response = (supabase.table("players")
+                .select("*")
+                .eq("login", login)
+                .eq("password", password)
+                .execute())
+    return response
+
+def get_ratings():
+    response = (supabase.table("players")
+                .select("name, rating")
+                .order("rating", desc=True)
+                .execute())
+    return response
